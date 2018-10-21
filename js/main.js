cc0242cf8b696bd179a1ee4fad05dd6fa56a4725
Marce Ramirez <marceram87@gmail.com>
2018-10-21 16:59:34 +0200

    Refactoring translation. Separating files by sections

--- js/main.js
+++ js/main.js
@@ -3,47 +3,48 @@
 /* Code for i18next
  *
  */
-$(document).ready(function(){
-    var browserLanguage = Cookies.get('locale') || navigator.language;
+$(document).ready(function () {
+  var browserLanguage = Cookies.get('locale') || navigator.language;
 
-    // set default language
-    setLanguage(browserLanguage);
-    
-    // action change language
-    $(this).on("click", "#languages", function(event) {
-        var language = $(event.target).attr("language");
-        setLanguage(language);
-    });
+  // set default language
+  setLanguage(browserLanguage);
+
+  // action change language
+  $(this).on("click", "#languages", function (event) {
+    var language = $(event.target).attr("language");
+    setLanguage(language);
+  });
 });
 
 function setLanguage(language) {
-    language = whitelistLanguage(language);
-    persistLanguagePreference(language);
+  // debugger;
+  language = whitelistLanguage(language);
+  persistLanguagePreference(language);
 
-    i18n.init({
-        lng: language,
-        fallbackLng: false,
-				ns: {
-             namespaces: ['translation', 'footer'],
-             defaultNs: 'translation'
-          },
-        debug: false
-    }, function() {
-        $("[data-i18n]").i18n();
-    });
+  i18n.init({
+    lng: language,
+    fallbackLng: false,
+    ns: {
+      namespaces: ['main', 'description', 'users', 'features', 'sponsors', 'footer'],
+      defaultNs: 'main'
+    },
+    debug: false
+  }, function () {
+    $("[data-i18n]").i18n();
+  });
 }
 
 function persistLanguagePreference(language) {
-    Cookies.set('locale', language);
+  Cookies.set('locale', language);
 }
 
 function whitelistLanguage(language) {
-    var available_languages = ["ca", "en", "es", "pt"];
-    var default_language = "es";
+  var available_languages = ["ca", "en", "es", "pt"];
+  var default_language = "ca";
 
-    if (available_languages.indexOf(language) == -1) {
-      return default_language;
-    }
+  if (available_languages.indexOf(language) == -1) {
+    return default_language;
+  }
 
-    return language;
+  return language;
 }
